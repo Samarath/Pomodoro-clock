a1a885b6fd4c8b70aa71f817bb9225d58bc6e953
Samrath <samrathkumarmuz@gmail.com>
2020-05-05 10:57:00 +0530

    Updates

---
+++ precache-manifest.d419358f678095615498bc7356985aa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0911d6ead39284b4c95e2b42e4273e",
+    "url": "/Pomodoro-clock/index.html"
+  },
+  {
+    "revision": "fd3e7a57b6840ca68e2a",
+    "url": "/Pomodoro-clock/static/css/main.b10735e3.chunk.css"
+  },
+  {
+    "revision": "6a4476bb0e263ac05a06",
+    "url": "/Pomodoro-clock/static/js/2.168bc9e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-clock/static/js/2.168bc9e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3e7a57b6840ca68e2a",
+    "url": "/Pomodoro-clock/static/js/main.ee427a57.chunk.js"
+  },
+  {
+    "revision": "aacba62c88155c802367",
+    "url": "/Pomodoro-clock/static/js/runtime-main.80ba24fd.js"
+  },
+  {
+    "revision": "b09e4c0545d09ee5b0c985fc6a05c54c",
+    "url": "/Pomodoro-clock/static/media/GenghisKhan.b09e4c05.otf"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/Pomodoro-clock/static/media/digital-7.58045dab.ttf"
+  }
+]);
